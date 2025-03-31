5e91e69856dfe6ffba77b3aef3bf8a290ec908a1
Sivakumaran M <mskmss1516@gmail.com>
2025-03-31 14:30:41 +0530

    Learning Riverpod -6

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,17 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_files/models/product.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
   // initial value
   @override
   Set<Product> build() {
-    return const {
-      Product(
-          id: '4',
-          title: 'Product 4',
-          price: 400,
-          image: 'assets/products/backpack.png')
-    };
+    return const {};
   }
 
   // methods to update state
@@ -28,6 +26,6 @@ class CartNotifier extends Notifier<Set<Product>> {
   }
 }
 
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
-  return CartNotifier();
-});
+// final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
+//   return CartNotifier();
+// });
